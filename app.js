5d165b8d246def0db27d6bb904acec0a8fe90fc8
Truong Duc Khai <duckhai80@gmail.com>
2024-09-26 01:20:05 +0700

    Handling POST request

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on the ${PORT}.`);
 });
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({
 //     message: 'Hello from the server',
@@ -31,4 +33,20 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-console.log(tours);
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
+});
