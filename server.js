34609056c44d545b38c20dae400c65599d7e4d5e
Truong Duc Khai <duckhai80@gmail.com>
2024-09-29 23:46:21 +0700

    Connecting database with express app

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
+const DATABASE = process.env.DATABASE_REMOTE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DATABASE, {}).then(() => {
+  console.log('Database connect successful!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on the ${PORT}.`);
