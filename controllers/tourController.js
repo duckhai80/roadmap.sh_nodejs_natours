a64c72fe969011adba1ff9e1a98ed9f1d11e5ca7
Truong Duc Khai <duckhai80@gmail.com>
2024-10-01 01:02:24 +0700

    Updating documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,8 +21,8 @@ exports.getAllTours = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-    // const tour = await Tour.findById(req.params.id);
-    const tour = await Tour.findOne({ _id: req.params.id });
+    const tour = await Tour.findById(req.params.id);
+    // const tour = await Tour.findOne({ _id: req.params.id });
 
     res.status(200).json({
       status: 'success',
@@ -49,20 +49,36 @@ exports.createTour = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(404).json({
+    res.status(400).json({
       status: 'fail',
       message: error,
     });
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Update tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    // const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
