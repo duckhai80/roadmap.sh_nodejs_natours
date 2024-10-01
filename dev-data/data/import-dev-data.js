533ae082f50673048168d363ca095f787858c6b6
Truong Duc Khai <duckhai80@gmail.com>
2024-10-01 22:44:09 +0700

    Importing development data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: 'config.env' });
+
+const DATABASE = process.env.DATABASE_REMOTE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DATABASE).then(() => {
+  console.log('Connect database successful!');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+};
+
+switch (process.argv[2]) {
+  case '--import-data':
+    importData();
+    break;
+  case '--delete-data':
+    deleteData();
+    break;
+  default:
+    break;
+}
+
+console.log(process.argv);
