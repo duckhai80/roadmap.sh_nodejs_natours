973c02dfd635b2b16090c9e868041f03b7b0099e
Truong Duc Khai <duckhai80@gmail.com>
2024-10-04 01:27:22 +0700

    Making pagination API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,6 +36,20 @@ exports.getAllTours = async (req, res) => {
       tourQuery = tourQuery.select('-__v');
     }
 
+    //  5. Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    tourQuery = tourQuery.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const allTours = await Tour.countDocuments();
+
+      if (skip >= allTours) throw new Error('This page does not exit!');
+    }
+
+    //  Execute query
     const tours = await tourQuery;
 
     res.status(200).json({
