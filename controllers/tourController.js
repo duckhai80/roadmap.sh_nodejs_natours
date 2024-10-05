a07ecbc85d535e2eeda52a078c49cea0b41ff2c9
Truong Duc Khai <duckhai80@gmail.com>
2024-10-06 00:04:35 +0700

    Making alias API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,15 @@
 const Tour = require('../models/tourModel');
 
+// name,price,ratingsAverage,summary,difficulty
+
+exports.get5CheapTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,sort';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     //  1.a. Filtering
