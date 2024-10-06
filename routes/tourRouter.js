34c15b5a819106a9cb62e252830bd8d9af63eca2
Truong Duc Khai <duckhai80@gmail.com>
2024-10-07 00:07:18 +0700

    Aggregation pipeline with matching and grouping

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -9,6 +9,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.get5CheapTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
