7af15ccec1fb4a9b31eabf0a0372ee1a6eb6183f
Truong Duc Khai <duckhai80@gmail.com>
2024-10-14 22:58:51 +0700

    Document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -62,6 +64,18 @@ const tourSchema = mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+//  Document middleware: run with .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lowercase: true });
+
+  next();
+});
+
+tourSchema.post('save', (doc, next) => {
+  console.log(doc);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
