dddb67baf32e997ee195898ff6b964e73c2bd3ae
Truong Duc Khai <duckhai80@gmail.com>
2024-10-14 23:27:55 +0700

    Query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -75,6 +79,21 @@ tourSchema.pre('save', function (next) {
 
 tourSchema.post('save', (doc, next) => {
   console.log(doc);
+
+  next();
+});
+
+//  Query middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query tooks ${Date.now() - this.start} milliseconds.`);
+
   next();
 });
 
