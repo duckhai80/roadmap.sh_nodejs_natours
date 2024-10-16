efb14661f40a57a4da5041306a83a312d8cab2a0
Truong Duc Khai <duckhai80@gmail.com>
2024-10-16 22:50:17 +0700

    Aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -97,6 +97,15 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+//  Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
