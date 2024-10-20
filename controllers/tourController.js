1cdd2c2035755316a61272d6ea4c40ae89eec49f
Truong Duc Khai <duckhai80@gmail.com>
2024-10-20 17:10:56 +0700

    Adding 404 not found errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
 
 exports.get5CheapTours = (req, res, next) => {
   req.query.limit = '5';
@@ -30,6 +31,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // const tour = await Tour.findOne({ _id: req.params.id });
+
+  if (!tour) {
+    return next(new AppError('Can not found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -69,7 +74,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('Can not found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
