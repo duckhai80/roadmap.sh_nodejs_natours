1861794c1fb1543d724167780473a4079b4a6bba
Truong Duc Khai <duckhai80@gmail.com>
2024-10-20 17:31:58 +0700

    Errors during development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //  Operational, trusted error
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  //  Programming or other unknown error
+  else {
+    console.log('ERROR 💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went vert wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
