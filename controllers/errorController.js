a68b9ea33c093a4d7365a3c2134950a4db7f6354
Truong Duc Khai <duckhai80@gmail.com>
2024-10-21 23:17:36 +0700

    Handling invalid database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,11 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -35,6 +43,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    if (err.name === 'CastError') {
+      err = handleCastErrorDB(err);
+    }
+
     sendErrorProd(err, res);
   }
 };
