8340a9a64f0c439c003a8afe1942c0e381e238bd
Truong Duc Khai <duckhai80@gmail.com>
2025-04-09 01:20:05 +0700

    Deleting the current user

--- models/userModel.js
+++ models/userModel.js
@@ -49,6 +49,17 @@ const userSchema = mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+userSchema.pre(/^find/, async function (next) {
+  this.find({ active: { $ne: false } });
+
+  next();
 });
 
 userSchema.pre('save', async function (next) {
