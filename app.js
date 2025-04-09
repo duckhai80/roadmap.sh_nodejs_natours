45c6ebcfc6b9e560c9aa1917ceb230dfadea0a2c
Truong Duc Khai <duckhai80@gmail.com>
2025-04-09 16:23:16 +0700

    Data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 // Parse body, read data from body of req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xssClean());
 
 // Serve static files
 app.use(express.static(`${__dirname}/public`));
