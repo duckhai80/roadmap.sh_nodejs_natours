76c7580c1894fe5a1f4b6772c320b59700378a7c
Truong Duc Khai <duckhai80@gmail.com>
2025-05-01 18:18:44 +0700

    Extending our base template with blocks

--- app.js
+++ app.js
@@ -83,6 +83,16 @@ app.get('/', (req, res) => {
     user: 'Khai Truong',
   });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All tours',
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
