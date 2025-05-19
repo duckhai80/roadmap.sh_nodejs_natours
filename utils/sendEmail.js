18941264745d75f040bb30ddfea11fd03b64dad4
Truong Duc Khai <duckhai80@gmail.com>
2025-05-19 19:35:44 +0700

    Building a complex email handler

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,48 @@
+const pug = require('pug');
 const nodemailer = require('nodemailer');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.to = user.email;
+    this.from = `Jonas <${process.env.EMAIL_FROM}>`;
+  }
 
-  const mailOptions = {
-    from: 'Jonas <admin@jonas.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: '',
-  };
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
 
-  await transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // Render template based on a bug template
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}`);
+
+    // Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    };
+
+    // Create a transport and send email
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours family!');
+  }
 };
-
-module.exports = sendEmail;
