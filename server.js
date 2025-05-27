70ba2f5982ad2d47e0b6a21d1a3746c8080be4a6
Truong Duc Khai <duckhai80@gmail.com>
2025-05-27 21:52:28 +0700

    Responding to a SIGTERM signal

--- server.js
+++ server.js
@@ -34,3 +34,11 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully!');
+
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
