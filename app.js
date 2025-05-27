bc95b679d033517b14392d6a61deb79045d2dac0
Truong Duc Khai <duckhai80@gmail.com>
2025-05-27 22:48:17 +0700

    Implementing CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xssClean = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const viewRouter = require('./routes/viewRouter');
@@ -24,6 +25,17 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //  1. MIDDLEWARE
+// Implement CORS
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   }),
+// );
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
