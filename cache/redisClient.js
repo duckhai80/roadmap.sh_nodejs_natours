b171fbe84a72d0783c50e5d1997622d06e9f44bc
Truong Duc Khai <duckhai80@gmail.com>
2025-06-22 09:26:34 +0700

    Updating cache using Redis Upstash

--- cache/redisClient.js
+++ cache/redisClient.js
@@ -1,13 +1,16 @@
 const redis = require('redis');
 
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const redisClient = redis.createClient(REDIS_PORT);
+const REDIS_URL =
+  process.env.REDIS_UPSTASH_URL || process.env.REDIS_PORT || 6379;
+const redisClient = redis.createClient({
+  url: REDIS_URL,
+});
 
-// redisClient
-//   .connect()
-//   .then(async () => {
-//     console.log('Redis connect successful!');
-//   })
-//   .catch((error) => console.log(error));
+redisClient
+  .connect()
+  .then(async () => {
+    console.log('Redis connect successful!');
+  })
+  .catch((error) => console.log(error));
 
 module.exports = redisClient;
